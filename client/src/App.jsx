69f6f89cc5f1b070cbf560d8e0deab61baa5185e
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-08 12:25:00 +0700

    feat: add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css'
+import "./App.css";
 import { BrowserRouter } from "react-router-dom";
-import AuthenticatedApp from './pages/authenticated-app';
+import AuthenticatedApp from "./pages/authenticated-app";
 
 function App() {
-
   return (
     <div>
       <BrowserRouter>
         <AuthenticatedApp />
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
