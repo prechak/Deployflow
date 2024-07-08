1abb3af429f7271eb931d3ab966f8c4a98c06a59
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-08 12:25:00 +0700

    feat: add login page

---
+++ client/src/pages/UserCourse.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function UserCourse() {
+  return (
+    <>
+      <div className="bg-Gray-100">
+        <div className="text-black text-3xl">User Courses</div>
+      </div>
+    </>
+  );
+}
+
+export default UserCourse;
