e3a6926cafe386749a43e65927e13b0cca138d75
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-09 16:42:26 +0700

    style: edit bg and decorations

--- client/src/pages/non-authorized/loginpage.jsx
+++ client/src/pages/non-authorized/loginpage.jsx
@@ -7,10 +7,8 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     console.log("Email:", email);
     console.log("Password:", password);
-
     setEmail("");
     setPassword("");
   };
@@ -24,9 +22,10 @@ function Login() {
           Test Navbar
         </Link>
       </header>
-      <div className="text-black relative flex flex-col justify-center items-center h-screen bg-Gray-100">
-        <div className="absolute right-0 top-9">
+      <div className="flex flex-col justify-center items-center h-screen w-screen bg-Gray-100 relative overflow-hidden">
+        <div className="absolute right-0 top-9 md:top-2 max-w-screen ">
           <svg
+            className="md:w-[133px] md:h-[500px]"
             width="33"
             height="117"
             viewBox="0 0 33 117"
@@ -39,40 +38,9 @@ function Login() {
             />
           </svg>
         </div>
-        <div className="absolute top-[270px] right-4">
+        <div className="absolute left-0 bottom-0 max-w-screen md:left-[-2rem] ">
           <svg
-            width="11"
-            height="9"
-            viewBox="0 0 11 9"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M9.00977 4.5C9.00977 6.0121 7.59533 7.5 5.50977 7.5C3.4242 7.5 2.00977 6.0121 2.00977 4.5C2.00977 2.9879 3.4242 1.5 5.50977 1.5C7.59533 1.5 9.00977 2.9879 9.00977 4.5Z"
-              stroke="#F47E20"
-              stroke-width="3"
-            />
-          </svg>
-        </div>
-        <div className="absolute left-0 top-16">
-          <svg
-            width="16"
-            height="31"
-            viewBox="0 0 16 31"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="0.272194"
-              cy="15.681"
-              r="14.9192"
-              transform="rotate(-75 0.272194 15.681)"
-              fill="#C6D6EF"
-            />
-          </svg>
-        </div>
-        <div className="absolute left-0 bottom-0">
-          <svg
+            className="md:w-[100px] md:h-[400px]"
             width="29"
             height="108"
             viewBox="0 0 29 108"
@@ -85,55 +53,106 @@ function Login() {
             />
           </svg>
         </div>
+        <div className="absolute top-[370px] right-[6rem] md:top-[470px] md:right-10">
+          <svg
+            className="md:w-[50px] md:h-auto stroke-[3px] md:stroke-[0.5px]"
+            width="11"
+            height="9"
+            viewBox="0 0 11 9"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.00977 4.5C9.00977 6.0121 7.59533 7.5 5.50977 7.5C3.4242 7.5 2.00977 6.0121 2.00977 4.5C2.00977 2.9879 3.4242 1.5 5.50977 1.5C7.59533 1.5 9.00977 2.9879 9.00977 4.5Z"
+              stroke="#F47E20"
+            />
+          </svg>
+        </div>
+        <div className="absolute left-[-40px] top-[20px] md:left-[4rem] md:top-[8rem] ">
+          <svg
+            width="74"
+            height="74"
+            viewBox="0 0 74 74"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="36.7032"
+              cy="36.7032"
+              r="36.5"
+              transform="rotate(-75 36.7032 36.7032)"
+              fill="#C6D6EF"
+            />
+          </svg>
+        </div>
+        <div className="absolute top-0 md:top-[15rem] md:left-[10rem]">
+          <svg
+            width="23"
+            height="23"
+            viewBox="0 0 23 23"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.843 1.99998L8.83754 20.6805"
+              stroke="#2FAC61"
+              stroke-width="3"
+              stroke-linecap="round"
+            />
+            <path
+              d="M1.99986 8.83751L20.6804 13.8429"
+              stroke="#2FAC61"
+              stroke-width="3"
+              stroke-linecap="round"
+            />
+          </svg>
+        </div>
 
         <form onSubmit={handleSubmit}>
-          <div className="flex flex-col gap-7 w-[22rem] md:pt-20 md:w-[30rem] md:gap-8 ">
+          <div className="flex flex-col gap-7 w-[22rem] md:pt-20 md:w-[30rem] md:gap-8 text-black">
             <h1 className="text-[#22269E] text-3xl font-medium pb-2  md:text-4xl">
               Welcome back!
             </h1>
             <div className="container md:font-medium">
-              <label>
-                Email
-                <p>
-                  <input
-                    type="email"
-                    className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] outline-none block w-full p-3"
-                    placeholder="Enter Email"
-                    onChange={(e) => {
-                      setEmail(e.target.value);
-                    }}
-                    value={email}
-                    required
-                  />
-                </p>
-              </label>
+              <label htmlFor="email">Email</label>
+              <p>
+                <input
+                  type="email"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-300  block w-full p-3 "
+                  required
+                  placeholder="Enter Email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  value={email}
+                />
+              </p>
             </div>
-            <div className="container pb-2 md:font-medium">
-              <label>
-                Password
-                <p>
-                  {" "}
-                  <input
-                    type="password"
-                    className=" border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] outline-none block w-full p-3"
-                    placeholder="Enter Password"
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                    value={password}
-                    required
-                  />
-                </p>
-              </label>
+
+            <div className="container pb-2 md:font-medium text-black">
+              <label htmlFor="password">Password</label>
+              <p>
+                {" "}
+                <input
+                  type="password"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-300  block w-full p-3"
+                  placeholder="Enter Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  value={password}
+                  required
+                />
+              </p>
             </div>
 
             <button
               type="submit"
-              className="text-white bg-Blue-500 font-medium rounded-xl text-sm w-full sm:w-auto px-4 py-4 text-center hover:bg-Blue-400"
+              className="text-white bg-Blue-500 font-medium rounded-xl text-sm w-full sm:w-auto px-4 py-4 text-center hover:bg-Blue-400  transition duration-250 ease-in-out"
             >
               Log in
             </button>
-            <p className="pt-6">
+            <p className="pt-6 text-black">
               Don't have an account?{" "}
               <Link
                 to="/register"
