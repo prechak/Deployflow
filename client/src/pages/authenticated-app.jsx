0c416c0bb564fd3c27a45f3ddd9e4af3864db2a8
Krittayapornn <krittayaporn.ww@gmail.com>
2024-07-09 21:02:51 +0700

    feat: create file user-confirmation and user-coursedetail

--- client/src/pages/authenticated-app.jsx
+++ client/src/pages/authenticated-app.jsx
@@ -4,21 +4,26 @@ import Coursedetail from "./non-authorized/coursedetail";
 import HomePage from "./non-authorized/homepage";
 import LoginAdmin from "./admin/login-admin";
 import AddCourseAdmin from "./admin/addcourse-admin";
+import Modal from "../components/coursedetail/modal";
+import ConfirmationCourse from "./authorized/user-confirmationcourse";
 
 function AuthenticatedApp() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/coursedetail" element={<Coursedetail />} />
-        <Route path="/user/coursedetail" element={<UserCoursedetail />} />
-        <Route path="/login/admin" element={<LoginAdmin />} />
-        <Route path="/admin/addcourse/" element={<AddCourseAdmin />} />
-      </Routes>
-    </div>
-  );
-}
 
-export default AuthenticatedApp;
+    return (
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/coursedetail" element={<Coursedetail />} />
+          <Route path="/user/coursedetail" element={<UserCoursedetail />} />
+          <Route path="/user/confirmationcourse" element={<ConfirmationCourse />} />
+          <Route path="/modal" element={<Modal />} />
+          <Route path="/login/admin" element={<LoginAdmin />} />
+          <Route path="/admin/addcourse/" element={<AddCourseAdmin />} />
+        </Routes>
+      </div>
+    );
+  }
+  
+  export default AuthenticatedApp;
