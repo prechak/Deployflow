3f627b0247f3a4d71e8f9115cec791d9e09485a5
Krittayapornn <krittayaporn.ww@gmail.com>
2024-07-09 11:03:39 +0700

    feat: created file coursedetail from folder non-authorized

--- client/src/pages/authenticated-app.jsx
+++ client/src/pages/authenticated-app.jsx
@@ -2,21 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./non-authorized/loginpage";
 import Coursedetail from "./non-authorized/coursedetail";
 import HomePage from "./non-authorized/homepage";
-import LoginAdmin from "./admin/login-admin";
-import AddCourseAdmin from "./admin/addcourse-admin";
+import LoginAdmin from "./admin/login-admin"
+import AddCourseAdmin from "./admin/addcourse-admin"
+import UserCoursedetail from "./authorized/user-coursedetail";
 
 function AuthenticatedApp() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/coursedetail" element={<Coursedetail />} />
-        <Route path="/login/admin" element={<LoginAdmin />} />
-        <Route path="/admin/addcourse/" element={<AddCourseAdmin />} />
-      </Routes>
-    </div>
-  );
-}
-
-export default AuthenticatedApp;
+    return (
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/coursedetail" element={<Coursedetail />} />
+          <Route path="/user/coursedetail" element={<UserCoursedetail />} />
+          <Route path="/login/admin" element={<LoginAdmin />} />
+          <Route path="/admin/addcourse" element={<AddCourseAdmin />} />
+        </Routes>
+      </div>
+    );
+  }
+  
+  export default AuthenticatedApp;
