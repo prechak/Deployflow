2df09edcba6387f851459f1b15ddea3a594a3d40
Precha Krasaesup <main@Prechas-Mac-Pro.local>
2024-07-11 17:31:57 +0700

    feat: setup admin login and demo admin page login

--- client/src/pages/admin/login-admin.jsx
+++ client/src/pages/admin/login-admin.jsx
@@ -1,12 +1,108 @@
+import * as React from "react";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function LoginAdmin() {
-    return (
-      <div>
-        <div>
-          <nav className="text-red-500">LoginAdmin</nav>
+  // FOR TEST ONLY
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [state, setState] = useState({
+    loading: null,
+    error: null,
+    user: null,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const result = await axios.post(
+        "http://localhost:4000/admin/login",
+        { email, password } // Pass email and password as object
+      );
+
+      const token = result.data.token;
+      localStorage.setItem("token", token);
+
+      const userDataFromToken = jwtDecode(token);
+      setState({ ...state, user: userDataFromToken });
+      navigate("/");
+    } catch (error) {
+      setState({ ...state, error: error.message });
+    }
+  };
+
+  return (
+    <>
+      <section className="bg-blue-100 flex items-center justify-center min-h-screen">
+        <div className="min-h-screen flex items-center justify-center bg-blue-100">
+          <div className="bg-white p-6 rounded shadow-md w-full max-w-sm">
+            <h2 className="text-2xl text-black font-bold mb-6 text-center">
+              Login Admin Test
+            </h2>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-4">
+                <label
+                  className=" block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                  Email
+                </label>
+                <input
+                  className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <input
+                  className="bg-white shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                />
+                <p className="text-blue-500 text-xs italic">Forgot password?</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  type="submit"
+                >
+                  Login
+                </button>
+                <a
+                  className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                  href="#"
+                >
+                  Signup
+                </a>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    );
-  }
-  
-  export default LoginAdmin;
-  
+      </section>
+    </>
+  );
+}
+
+export default LoginAdmin;
--- server/app.mjs
+++ server/app.mjs
@@ -3,6 +3,8 @@ import cors from "cors";
 import courseRouter from "./routes/course.mjs";
 import userRouter from "./routes/user.mjs";
 import connectionPool from "./utils/db.mjs";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
@@ -23,6 +25,55 @@ connect();
 
 app.use("/courses", courseRouter);
 app.use("/users", userRouter);
+
+//admin login
+app.post("/admin/login", async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).json({ message: `Email and Password are required` });
+  }
+
+  const role = req.body.role ? req.body.role.toLowerCase().trim() : "";
+
+  if (role === "admin") {
+    return res.status(401).json({ message: `Only admin can access` });
+  }
+
+  try {
+    const { rows } = await connectionPool.query(
+      `select * from users where email = $1`,
+      [req.body.email]
+    );
+
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Email does not exist." });
+    }
+
+    const user = rows[0];
+    // console.log(user);
+
+    // verify the password
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid password." });
+    }
+
+    // generate token
+    const secretKey = process.env.SECRET_KEY;
+    const token = jwt.sign(
+      { id: user.userid, email: user.email, role: user.role },
+      secretKey,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res.status(200).json({ message: "Login successful.", token });
+  } catch {
+    console.error("Error during admin login:");
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
 
 //Server connection test
 app.get("/test", (req, res) => {
