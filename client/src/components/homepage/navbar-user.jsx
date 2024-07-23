11022a2c63e5e6b2e51f807f2defc9ad6bfd44a8
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-07-23 14:03:56 +0700

    feat:navbar user link desire

--- client/src/components/homepage/navbar-user.jsx
+++ client/src/components/homepage/navbar-user.jsx
@@ -169,7 +169,7 @@ function NavbarUser() {
                     My Homework
                   </Link>
                   <Link
-                    to=""
+                    to="/user/desire"
                     className="flex px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex-row"
                     role="menuitem"
                   >
