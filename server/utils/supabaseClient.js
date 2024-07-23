221735fbcf754bdc1fd98b9feb8edc2e1e73d9ae
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-07-23 14:00:17 +0700

    feat:delete supabase server

--- server/utils/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// supabaseClient.js
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
