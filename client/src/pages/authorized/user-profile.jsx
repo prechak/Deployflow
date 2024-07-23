a4e485d371fd616675416427955e9d042084629a
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-07-23 14:16:51 +0700

    style: edit md size responsive

--- client/src/pages/authorized/user-profile.jsx
+++ client/src/pages/authorized/user-profile.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, Navigate } from "react-router-dom";
-import Navbarnonuser from "../../components/homepage/navbar-user";
+import NavbarUser from "../../components/homepage/navbar-user";
 import Footer from "../../components/homepage/footer";
 import axios from "axios";
 import { useAuth } from "../../contexts/authentication";
@@ -215,12 +215,15 @@ function EditProfileForm() {
 
   return (
     <>
-      <Navbarnonuser />
+      <div className="sticky top-0 bg-white z-40">
+        <NavbarUser />
+      </div>
+
       <header className="text-black font-medium text-Headline3 pt-[3rem] md:text-Headline2 md:pb-[3rem] flex flex-col justify-center items-center">
         Profile
       </header>
       <form
-        className="flex flex-col md:flex-row justify-center items-center md:gap-[3rem] md:mr-[2rem] md:-ml-[8rem] mb-[14rem]"
+        className="flex flex-col md:flex-row justify-center items-center md:gap-[3rem] mb-[14rem]"
         onSubmit={handleSubmit}
       >
         <div className="py-8">
