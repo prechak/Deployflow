1b9ff671eb41fa74ff4950cfcda40bb9023a96c2
Precha Krasaesup <main@Prechas-Mac-Pro.local>
2024-08-07 00:37:41 +0700

    set: config cors

--- server/app.mjs
+++ server/app.mjs
@@ -11,9 +11,14 @@ import submissionRouter from "./routes/submission.mjs";
 import subscriptionRouter from "./routes/subscriptions.mjs";
 import assignmentRouter from "./routes/assignments.mjs";
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: "https://deployflow.vercel.app", // Allow your client origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+
 const port = process.env.PORT || 4000;
 
 //Connection test
